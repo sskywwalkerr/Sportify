60eabbc3536b921d646abc3c3a157bc505c7c1ff
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-10-21 19:04:25 +0500

    created models for the user

---
+++ src/app/user/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, Integer, DateTime, Boolean, sql
+from src.db.session import Base
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, index=True, unique=True)
+    username = Column(String, unique=True)
+    email = Column(String, unique=True)
+    password = Column(String)
+    first_name = Column(String(150))
+    last_name = Column(String(150))
+    date_join = Column(DateTime(timezone=True), server_default=sql.func.now())
+    last_login = Column(DateTime)
+    is_active = Column(Boolean, default=True)
+    is_staff = Column(Boolean, default=True)
+    is_superuser = Column(Boolean, default=True)
+    avatar = Column(String)
