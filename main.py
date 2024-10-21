96debfc50fb9ea8f3e123b72b55d45d543e05f18
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-10-21 19:04:56 +0500

    middleware

--- main.py
+++ main.py
@@ -2,31 +2,31 @@ import uvicorn
 from fastapi import FastAPI, Response, Request
 from fastapi.middleware.cors import CORSMiddleware
 
-# from src.config import confing
-# from src.app import routers
-# from src.db.session import SessionLocal
+from src.config import settings
+#from src.app import routers
+from src.db.session import SessionLocal
 
 
 app = FastAPI(
-    title="SportifyFast",
+    title=" Fast",
     description="Author - Sky",
     version="0.1.0",
 )
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# @app.middleware("http")
-# async def db_session_middleware(request: Request, call_next):
-#     responce = Response("Internal Server Error", status_code=500)
-#     try:
-#         request.state.db = SessionLocal()
-#         response = await call_next(request)
-#     finally:
-#         request.state.db.close()
-#     return response
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next):
+    responce = Response("Internal Server Error", status_code=500)
+    try:
+        request.state.db = SessionLocal()
+        response = await call_next(request)
+    finally:
+        request.state.db.close()
+    return response
