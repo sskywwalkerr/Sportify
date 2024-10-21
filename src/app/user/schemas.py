7f94473127954412ee64393d52b71a72ab8317cd
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-10-21 20:12:43 +0500

    created a schemas

--- src/app/user/schemas.py
+++ src/app/user/schemas.py
@@ -19,17 +19,16 @@ class UserCreate(UserBaseInDB):
     """ Свойства для получения через API при создании из админки
     """
     username: str
-    email: EmailStr
+    email: str
     password: str
     first_name: str
-    avatar: str = None
 
 
 class UserCreateInRegistration(BaseModel):
     """ Свойства для получения через API при регистрации
     """
     username: str
-    email: EmailStr
+    email: str
     password: str
     first_name: str
     avatar: str = None
@@ -41,59 +40,24 @@ class UserCreateInRegistration(BaseModel):
 class UserUpdate(UserBaseInDB):
     """ Properties to receive via API on update
     """
-    password: Optional[str] = Form(...)
+    password: Optional[str] = None
 
 
-# class User(UserBaseInDB):
-#     """ Additional properties to return via API
-#     """
-#     pass
+class User(UserBaseInDB):
+    """ Additional properties to return via API
+    """
+    pass
 
 
 class UserInDB(UserBaseInDB):
     """ Additional properties stored in DB
     """
-    # password: str
-
-
-class SocialAccount(BaseModel):
-    """ Schema social accounts
-    """
-    account_id: int
-    account_url: str
-    account_login: str
-    account_name: str
-    provider: str
-    user: UserCreateInRegistration
-
-    class Config:
-        orm_mode = True
-
-
-class SocialAccountGet(BaseModel):
-    """ Schema social accounts
-    """
-    account_id: int
-    account_url: str
-    account_login: str
-    account_name: str
-    provider: str
-    avatar_url: str
-
-    class Config:
-        orm_mode = True
-
+    password: str
 
 class UserPublic(UserBase):
     """ For public profile user
     """
     id: int
-    # social_account: SocialAccount
 
     class Config:
         orm_mode = True
-
-
-User_C_Pydantic = pydantic_model_creator(
-    User, name='create_user', exclude_readonly=True, exclude=('is_active', 'is_staff', 'is_superuser'))
-User_G_Pydantic = pydantic_model_creator(User, name='user')
