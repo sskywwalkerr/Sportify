24ed412d02895516e92779af40e5e8f7bc5e9635
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-10-23 14:00:37 +0500

    created a send_email

---
+++ src/app/auth/send_email.py
@@ -0,0 +1,62 @@
+from src.app.base.utils.email import send_email
+from src.config import settings
+from pathlib import Path
+
+
+def send_test_email(email_to: str):
+    """send test email"""
+    project_name = settings.PROJECT_NAME
+    subject = f"{project_name} - Test Email"
+    with open(Path(settings.EMAIL_TEMPLATES_DIR) / "test_email.html") as f:
+        template_str = f.read()
+    send_email(
+        email_to=email_to,
+        subject_template=subject,
+        html_template=template_str,
+        environment={"project_name": settings.PROJECT_NAME, "email": email_to}
+    )
+
+def send_reset_password_email(email_to: str, email: str, token: str):
+    """sending an email when the password is reset"""
+    project_name = settings.PROJECT_NAME
+    subject = f"{project_name} - Password recovery for user {email}"
+    with open(Path(settings.EMAIL_TEMPLATES_DIR) / "reset_password.html") as f:
+        template_str = f.read()
+    if hasattr(token, "decode"):
+        use_token = token.decode()
+    else:
+        use_token = token
+    server_host = settings.SERVER_HOST
+    link = f"{server_host}/reset-password?token={use_token}"
+    send_email(
+        email_to=email_to,
+        subject_template=subject,
+        html_template=template_str,
+        environment={
+            "project_name": settings.PROJECT_NAME,
+            "username": email,
+            "email": email_to,
+            "valid_hours": settings.EMAIL_RESET_TOKEN_EXPIRE_HOURS,
+            "link": link,
+        },
+    )
+
+def send_new_account_email(email_to: str, username: str, password: str, uuid:str):
+    """sending an email when the created user"""
+    project_name = settings.PROJECT_NAME
+    subject = f"{project_name} - New account for user {username}"
+    with open(Path(settings.EMAIL_TEMPLATES_DIR) / "new_account.html") as f:
+        template_str = f.read()
+    link = f"{settings.SERVER_HOST}/verify/?token={uuid}"
+    send_email(
+        email_to=email_to,
+        subject_template=subject,
+        html_template=template_str,
+        environment={
+            "project_name": settings.PROJECT_NAME,
+            "username": username,
+            "email": email_to,
+            "password": password,
+            "link": link,
+        },
+    )
