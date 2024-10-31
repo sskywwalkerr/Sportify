a7a92b4b86ea56e60eae5b38149dc16825b16601
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-10-31 16:15:10 +0500

    разделил api в service

--- src/app/auth/service.py
+++ src/app/auth/service.py
@@ -1,23 +1,58 @@
+import jwt
+from jwt.exceptions import InvalidTokenError
+from datetime import datetime, timedelta
+from typing import Optional
+
 from sqlalchemy.orm import Session
-from .models import Verification
 
+from .crud import auth_verify
+from .send_email import send_new_account_email
 
-class CRUDVerify:
-    """Создание и удаление ссылок подтверждения"""
-    def get(self, db_session: Session, uuid: str) -> Verification:
-        return db_session.query(Verification).filter(Verification.link == uuid).first()
+from src.app.user import schemas
+from src.app.user import crud
 
-    def create(self, db_session: Session, user: int) -> Verification:
-        db_obj = Verification(user_id=user)
-        db_session.add(db_obj)
-        db_session.commit()
-        db_session.refresh(db_obj)
-        return db_obj
+from src.config import settings
+from .schemas import TokenPayload, VerificationInDB
 
-    def remove(self, db_session: Session, uuid: str) -> Verification:
-        obj = db_session.query(Verification).filter(Verification.link == uuid).first()
-        db_session.delete(obj)
-        db_session.commit()
-        return obj
+password_reset_jwt_subject = "preset"
 
-auth_verify = CRUDVerify()
+def registration_user(new_user: schemas.UserCreateInRegistration, db: Session):
+    """Register a new user"""
+    if crud.user.get_by_username_email(db, username=new_user.username, email=new_user.email):
+        return True
+    else:
+        user = crud.user.create(db, obj_in=new_user)
+        verify = auth_verify.create(db, user.id)
+        send_new_account_email(new_user.email, new_user.username, new_user.password, verify.link)
+        return False
+
+def verify_registration_user(uuid: VerificationInDB, db: Session):
+    """confirmation of the user's email"""
+    verify = auth_verify.get(db, uuid.link)
+    if verify:
+        user = crud.user.get(db, verify.user_id)
+        crud.user.update(db, db_obj=user, obj_in=schemas.UserUpdate(**{"is_active": "True"}))
+        auth_verify.remove(db, uuid.link)
+        return True
+    else:
+        return False
+
+def generate_password_reset_token(email):
+    delta = timedelta(hours=settings.EMAIL_RESET_TOKEN_EXPIRE_HOURS)
+    now = datetime.utcnow()
+    expires = now + delta
+    exp = expires.timestamp()
+    encoded_jwt = jwt.encode(
+        {"exp": exp, "nbf": now, "sub": password_reset_jwt_subject, "email": email},
+        settings.SECRET_KEY,
+        algorithm="HS256",
+    )
+    return encoded_jwt
+
+def verify_password_reset_token(token) -> Optional[str]:
+    try:
+        decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+        assert decoded_token["sub"] == password_reset_jwt_subject
+        return decoded_token["email"]
+    except InvalidTokenError:
+        return None
