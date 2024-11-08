3b28124e97f38687319f614fad0e9713497b0808
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-11-08 12:34:33 +0500

    added new function create_social_account for github

--- src/app/user/service.py
+++ src/app/user/service.py
@@ -1,6 +1,25 @@
-from . import schemas
+from sqlalchemy.orm import Session
+
+from src.app.base.utils import generate
+
+from . import schemas, crud
 
 
-def create_social_account(profile: schemas.SocialAccount):
-    print(profile)
+def create_social_account(db: Session, profile: schemas.SocialAccount):
+    if crud.social_account.exists(db, account_id=profile.account_id):
+        account = crud.social_account.get(db, account_id=profile.account_id)
+        return account.user
+    new_user = schemas.UserCreate(
+        username=generate.generate_name(profile.account_login),
+        email="soc@mail.ru",
+        password=generate.generate_pass(),
+        first_name=profile.account_name,
+        is_active=True,
+        avatar=profile.avatar_url
+    )
+    user = crud.user.create(db, schema=new_user)
+    prof = profile.dict(exclude={"avatar_url"})
+    crud.social_account.create(db, schema=prof, user_id=user.id)
+    return user
+
 
